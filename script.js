14b4a85ce170515de99abf117a50f27697c4de33
htpeps <harrison.thomas.05@outlook.com>
2026-08-04 14:21:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,39 +4,28 @@ const basketItems = document.getElementById("basket-items");
 const basketCount = document.getElementById("basket-count");
 const checkout = document.getElementById("checkout");
 
-document.querySelectorAll(".card button").forEach(button => {
+console.log("Script loaded");
 
+document.querySelectorAll(".card button").forEach(button => {
     button.addEventListener("click", () => {
 
-        const card = button.parentElement;
+        const card = button.closest(".card");
         const product = card.querySelector("h3").innerText;
 
         basket.push(product);
 
-        updateBasket();
+        basketItems.innerHTML = basket.join("<br>");
+        basketCount.innerText = basket.length;
 
-    });
-
-});
-
-function updateBasket(){
-
-    basketItems.innerHTML = "";
-
-    basket.forEach(item => {
-        basketItems.innerHTML += item + "<br>";
-    });
-
-    basketCount.innerText = basket.length;
-
-    const message =
+        const message =
 `Hello HT Peptides,
 
 I'd like to order:
 
 ${basket.join("\n")}`;
 
-    checkout.href =
+        checkout.href =
 "https://wa.me/447456872851?text=" + encodeURIComponent(message);
 
-}
+    });
+});
